9acdb63de74f3b22f2a1b32c9ce83e1b0beaed33
lina1712 <dolliu0915@gmail.com>
2023-06-20 18:24:00 -0500

    practica de ruby

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get '/products/new', to: 'products#new', as: :new_product
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
+
+end
